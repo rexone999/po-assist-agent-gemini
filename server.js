4e2ff47f21b46af9d1ee3ce7a7e62d46ca225549
rexone999 <projectmajorvd25@gmail.com>
2025-06-10 17:05:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const { chat } = require('./utils/gemini');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
